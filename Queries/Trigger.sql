3292f0392618e92528e81daaf9051cfed24f9b02
Thang <Miracleiztb@gmail.com>
2024-01-07 01:13:30 +0700

    trigger

--- Queries/Trigger.sql
+++ Queries/Trigger.sql
@@ -1,37 +1,46 @@
---1. Trigger audit Trail on Customer Update
+--1. Trigger to enforce branch manager existence
 --trigger function
-CREATE OR REPLACE FUNCTION audit_customer_update()
+CREATE OR REPLACE FUNCTION validate_branch_manager()
 RETURNS TRIGGER AS $$
 BEGIN
-    INSERT INTO customer_audit (customer_id, changed_on, previous_data)
-    VALUES (OLD.customer_id, now(), ROW(OLD.*));
+    IF NEW.manager_id IS NOT NULL AND NOT EXISTS (SELECT 1 FROM cm_staff WHERE staff_id = NEW.manager_id) THEN
+        RAISE EXCEPTION 'Cannot assign non-existent staff as branch manager.';
+    END IF;
+
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
 --attach trigger
-CREATE TRIGGER trigger_customer_update
-AFTER UPDATE ON cm_customer
-FOR EACH ROW EXECUTE FUNCTION audit_customer_update();
+CREATE TRIGGER trigger_validate_branch_manager
+BEFORE INSERT OR UPDATE ON cm_branch
+FOR EACH ROW EXECUTE FUNCTION validate_branch_manager();
 --test
 
---2. Trigger update Inventory After Order
+--2. Trigger to prevent deleting a job associated with a existence staff
 --trigger function
-CREATE OR REPLACE FUNCTION update_inventory_after_order()
+CREATE OR REPLACE FUNCTION prevent_delete_job()
 RETURNS TRIGGER AS $$
+DECLARE
+    job_count INT;
 BEGIN
-    UPDATE cm_menu SET quantity = quantity - NEW.quantity
-    WHERE item_id = NEW.item_id;
-    RETURN NEW;
+    SELECT COUNT(*) INTO job_count FROM cm_staff WHERE job_id = OLD.job_id;
+
+    IF job_count > 0 THEN
+        RAISE EXCEPTION 'Cannot delete job record with job_id %; it is associated with existing staff members.', OLD.job_id;
+    END IF;
+
+    RETURN OLD;
 END;
 $$ LANGUAGE plpgsql;
---attach trigger
-CREATE TRIGGER trigger_update_inventory
-AFTER INSERT ON cm_order_item
-FOR EACH ROW EXECUTE FUNCTION update_inventory_after_order();
 
+-- Attach Trigger
+CREATE TRIGGER trigger_prevent_delete_job
+BEFORE DELETE ON cm_job
+FOR EACH ROW EXECUTE FUNCTION prevent_delete_job();
 --test
 
---3. trigger validate new staff age
+--3. Trigger validate new staff age
 --trigger function
 CREATE OR REPLACE FUNCTION validate_staff_age()
 RETURNS TRIGGER AS $$
@@ -47,25 +56,33 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER trigger_validate_staff_age
 BEFORE INSERT ON cm_staff
 FOR EACH ROW EXECUTE FUNCTION validate_staff_age();
-
 --test
 
---4. trigger automatically set discount for large orders
+--4. trigger automatically set discount for returning customer
 --trigger function
-CREATE OR REPLACE FUNCTION apply_large_order_discount()
+CREATE OR REPLACE FUNCTION apply_returning_discount()
 RETURNS TRIGGER AS $$
 BEGIN
-    IF NEW.total_amount > 1000 THEN -- Assuming total_amount is a column in cm_order
-        NEW.discount := 10; -- 10% discount
+    IF NEW.discount = 0 AND EXISTS (
+        SELECT 1
+        FROM cm_order
+        WHERE customer_id = NEW.customer_id 
+		AND order_id <> NEW.order_id 
+		AND date_order <> NEW.date_order
+    ) THEN
+        NEW.discount := COALESCE((SELECT MAX(discount) + 5 FROM cm_order WHERE customer_id = NEW.customer_id), 5);
     END IF;
+	IF NEW.discount >= 15 THEN
+		NEW.discount := 15;
+	END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
---attach trigger
-CREATE TRIGGER trigger_large_order_discount
+-- Attach trigger
+CREATE TRIGGER trigger_returning_customer_discount
 BEFORE INSERT OR UPDATE ON cm_order
-FOR EACH ROW EXECUTE FUNCTION apply_large_order_discount();
+FOR EACH ROW EXECUTE FUNCTION apply_returning_discount();
 --test
 
 --5. trigger cascade Delete for Order Items
